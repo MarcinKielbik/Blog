19b454e91ae22440896da80a5eaf236ffdae63ac
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-02-25 13:33:31 +0100

    Create BlogPost model

--- blog/api/models.py
+++ blog/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
