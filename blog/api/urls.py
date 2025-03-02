3e5c9ca2bd7f2ded2aa6f4368c55a55af6cfd74e
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-03-02 13:09:45 +0100

    Add API routes for BlogPost views

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog-post', views.BlogPostList.as_view(), name='blogpsot-view-create')
+    path('blog-post/', views.BlogPostListAdd.as_view(), name='blogpsot-view-create'),
+    path('blog-post/<int:pk>/', views.BlogPostListUpdateDelete.as_view(), name='update')
 ]
--- blog/api/views.py
+++ blog/api/views.py
@@ -4,6 +4,12 @@ from rest_framework import generics
 from .models import BlogPost
 from .serializer import BlogPostSerializers
 
-class BlogPostList(generics.ListCreateAPIView):
+class BlogPostListAdd(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializers
+    
+    
+class BlogPostListUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializers
+    lookup_field = "pk"
